04d61113b5365c84155d847f72628a917321f50c
twicks95 <teguh.wck95@gmail.com>
2021-05-31 09:22:13 +0700

    Update index routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,10 @@ const Route = express.Router()
 
 const authRouter = require('../modules/auth/auth_routes')
 const userRouter = require('../modules/user/user_routes')
+const contactRouter = require('../modules/contact/contact_routes')
 
 Route.use('/auth', authRouter)
 Route.use('/user', userRouter)
+Route.use('/contact', contactRouter)
 
 module.exports = Route
