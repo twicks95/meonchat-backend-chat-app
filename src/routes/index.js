eae19efaa3af5cbc7ca8231a3962a81c3f8153c5
twicks95 <teguh.wck95@gmail.com>
2021-06-01 18:04:18 +0700

    Update index routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,13 @@ const Route = express.Router()
 const authRouter = require('../modules/auth/auth_routes')
 const userRouter = require('../modules/user/user_routes')
 const contactRouter = require('../modules/contact/contact_routes')
+const roomRouter = require('../modules/room/room_routes')
+const chatRouter = require('../modules/chat/chat_routes')
 
 Route.use('/auth', authRouter)
 Route.use('/user', userRouter)
 Route.use('/contact', contactRouter)
+Route.use('/room', roomRouter)
+Route.use('/chat', chatRouter)
 
 module.exports = Route
